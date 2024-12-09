ae89eede7ffe9f59a00a81c4b8208b470187742c
loganfox <logantcodes@gmail.com>
2024-12-09 13:36:24 -0600

    Adds localization

--- tests/Unit/Jobs/ProcessNewIPAddressTest.php
+++ tests/Unit/Jobs/ProcessNewIPAddressTest.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Notification;
 use Zaengle\LaravelSecurityNotifications\Jobs\ProcessNewIPAddress;
 use Zaengle\LaravelSecurityNotifications\Models\Login;
 use Zaengle\LaravelSecurityNotifications\Notifications\LoginFromNewIP;
+use Zaengle\LaravelSecurityNotifications\Services\IPLocationData;
 use Zaengle\LaravelSecurityNotifications\Tests\Setup\Models\User;
 use function Pest\Laravel\assertDatabaseHas;
 
@@ -16,12 +16,13 @@ it('it processes a new ip address', function () {
     expect(Login::count())->toBe(0);
 
     (new ProcessNewIPAddress(
-        ipLocationData: [
-            'query' => '127.0.0.1',
-            'city' => 'Minneapolis',
-            'region' => 'MN',
+        ipLocationData: new IPLocationData([
+            'ipAddress' => '127.0.0.1',
             'countryCode' => 'US',
-        ],
+            'region' => 'MN',
+            'city' => 'Minneapolis',
+            'timezone' => 'America/Chicago',
+        ]),
         userId: $user->getKey(),
         userType: $user->getMorphClass(),
     ))->handle();
@@ -31,10 +32,11 @@ it('it processes a new ip address', function () {
         'user_id' => $user->getKey(),
         'user_type' => $user->getMorphClass(),
         'location_data' => json_encode([
-            'query' => '127.0.0.1',
-            'city' => 'Minneapolis',
-            'region' => 'MN',
+            'ipAddress' => '127.0.0.1',
             'countryCode' => 'US',
+            'region' => 'MN',
+            'city' => 'Minneapolis',
+            'timezone' => 'America/Chicago',
         ]),
     ]);
 
@@ -49,12 +51,13 @@ it('does not send notification if disabled', function () {
     $user = User::factory()->create();
 
     (new ProcessNewIPAddress(
-        ipLocationData: [
-            'query' => '127.0.0.1',
-            'city' => 'Minneapolis',
-            'region' => 'MN',
+        ipLocationData: new IPLocationData([
+            'ipAddress' => '127.0.0.1',
             'countryCode' => 'US',
-        ],
+            'region' => 'MN',
+            'city' => 'Minneapolis',
+            'timezone' => 'America/Chicago',
+        ]),
         userId: $user->getKey(),
         userType: $user->getMorphClass(),
         sendNewIpNotification: false,
@@ -65,10 +68,11 @@ it('does not send notification if disabled', function () {
         'user_id' => $user->getKey(),
         'user_type' => $user->getMorphClass(),
         'location_data' => json_encode([
-            'query' => '127.0.0.1',
-            'city' => 'Minneapolis',
-            'region' => 'MN',
+            'ipAddress' => '127.0.0.1',
             'countryCode' => 'US',
+            'region' => 'MN',
+            'city' => 'Minneapolis',
+            'timezone' => 'America/Chicago',
         ]),
     ]);
 
